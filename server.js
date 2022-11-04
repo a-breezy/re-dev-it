64e73c30909d44968707a76bd9d1f490a13053f6
a.breezy <ambrose.wilkins@gmail.com>
2022-11-04 10:40:10 -0400

    added first query and solved "econnrefused" error from mysql2

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,20 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = mysql.createConnection(
+	{
+		host: "127.0.0.1",
+		user: "root",
+		password: "",
+		database: "election",
+	},
+	console.log("Connected to election database")
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+	console.log(err);
+	console.log(rows);
+});
 
 app.use((req, res) => {
 	res.status(404).end();
